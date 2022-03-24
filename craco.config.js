766b3c6de768db5203f7ac2ac527b40a5a34e602
choks <choks0225@naver.com>
2022-03-24 22:56:28 +0900

    chore: craco 설정 및 버전호환 세팅

---
+++ craco.config.js
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const CracoAlias = require('craco-alias');
+module.exports = {
+	plugins: [
+		{
+			plugin: CracoAlias,
+			options: {
+				source: 'tsconfig',
+				baseUrl: './',
+				tsConfigPath: 'tsconfig.paths.json',
+			},
+		},
+	],
+};
