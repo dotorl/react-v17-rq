13dbce0a95a74b132e2e00addca09d1c3b2f2cde
choks <choks0225@naver.com>
2022-04-04 00:07:21 +0900

    test: framermotion 적용 테스트

--- src/components/DefaultPage.tsx
+++ src/components/DefaultPage.tsx
@@ -12,23 +12,24 @@ const DefaultPage = () => {
 					</h1>
 				</header>
 
-				<motion.div>
-					<section className="contents">
-						<dl>
-							<dt>내게 맞는 영상 추천!</dt>
-							<dd>
-								<span>70여 개의 실시간 채널, 영화, 해외시리즈, </span>
-								<span>애니메이션 등 20만 여 편의 동영상 중</span>
-								<br />
-								<span>내게 맞는 동영상을 추천해주는 앱 서비스</span>
-							</dd>
-						</dl>
-						<ContentDownlod />
-					</section>
-				</motion.div>
-				<div className="bg">
+				<section className="contents">
+					<dl>
+						<motion.dt layout animate={{ opacity: 1 }} initial={{ opacity: 0 }} exit={{ opacity: 0 }} transition={{ duration: 1 }}>
+							내게 맞는 영상 추천!
+						</motion.dt>
+						<motion.dd layout animate={{ opacity: 1 }} initial={{ opacity: 0 }} exit={{ opacity: 0 }} transition={{ duration: 2 }}>
+							<span>70여 개의 실시간 채널, 영화, 해외시리즈, </span>
+							<span>애니메이션 등 20만 여 편의 동영상 중</span>
+							<br />
+							<span>내게 맞는 동영상을 추천해주는 앱 서비스</span>
+						</motion.dd>
+					</dl>
+					<ContentDownlod />
+				</section>
+
+				<motion.div className="bg" layout animate={{ opacity: 1 }} initial={{ opacity: 0 }} exit={{ opacity: 0 }} transition={{ duration: 3 }}>
 					<div></div>
-				</div>
+				</motion.div>
 			</div>
 		</>
 	);
