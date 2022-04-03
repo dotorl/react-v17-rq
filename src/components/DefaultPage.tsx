9f76349fa5112ac0e2697824e18d043e7aaa4025
choks <choks0225@naver.com>
2022-04-03 23:59:50 +0900

    test: framer-motion 적용 테스트

--- src/components/DefaultPage.tsx
+++ src/components/DefaultPage.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ContentDownlod from './common/ContentDownlod';
+import { motion } from 'framer-motion/dist/framer-motion';
 
 const DefaultPage = () => {
 	return (
@@ -10,18 +11,21 @@ const DefaultPage = () => {
 						<span className="txt-hide">U+모바일TV</span>
 					</h1>
 				</header>
-				<section className="contents">
-					<dl>
-						<dt>내게 맞는 영상 추천!</dt>
-						<dd>
-							<span>70여 개의 실시간 채널, 영화, 해외시리즈, </span>
-							<span>애니메이션 등 20만 여 편의 동영상 중</span>
-							<br />
-							<span>내게 맞는 동영상을 추천해주는 앱 서비스</span>
-						</dd>
-					</dl>
-					<ContentDownlod />
-				</section>
+
+				<motion.div>
+					<section className="contents">
+						<dl>
+							<dt>내게 맞는 영상 추천!</dt>
+							<dd>
+								<span>70여 개의 실시간 채널, 영화, 해외시리즈, </span>
+								<span>애니메이션 등 20만 여 편의 동영상 중</span>
+								<br />
+								<span>내게 맞는 동영상을 추천해주는 앱 서비스</span>
+							</dd>
+						</dl>
+						<ContentDownlod />
+					</section>
+				</motion.div>
 				<div className="bg">
 					<div></div>
 				</div>
